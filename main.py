457b1c2a934e100b5f1aff7f595e030a0eedbd37
Vijaya Krishnan V <vijayakrishnan3105@gmail.com>
2024-08-03 17:05:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,75 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import random
+import os
 from art import logo
-print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-shift=shift%26
-encode=[]
-decode=[]
 
-value=True
-def encrypt(text, shift):
-    blank=[]
-    blank=alphabet[shift:]+alphabet[:shift]
-    for i in range(len(text)):
-        for j in range(len(alphabet)):
-            if text[i]==alphabet[j]:
-                encode.append(blank[j])
-        if text[i] not in alphabet:
-            encode.append(text[i])
-    output="".join(encode)
-    print(output)
 
-def decrypt(text, shift):
-    blank=[]
-    blank=alphabet[shift:]+alphabet[:shift]
-    for i in range(len(text)):
-        for j in range(len(alphabet)):
-            if text[i]==blank[j]:
-                decode.append(alphabet[j])
-        if text[i] not in blank:
-            decode.append(text[i])
-    output="".join(decode)
-    print(output)
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
 
-while value==True:  
 
-    if direction=="encode":
-        encrypt(text, shift)
-    elif direction=="decode":
-        decrypt(text, shift)
-    n=str(input("Do u want to continue? Y OR N\n")).lower()
-    if n=="y":
-        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-        text = input("Type your message:\n").lower()
-        shift = int(input("Type the shift number:\n"))
-        shift=shift%26
-    elif n=="n":
-        value=False
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+
+def compare(user_score, computer_score):
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose 😤"
+    if user_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > computer_score:
+        return "You win 😃"
     else:
-        print("Enter y or n correctly\n")
+        return "You lose 😤"
 
+
+def play_game():
+    print(logo)
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"   Your cards: {user_cards}, current score: {user_score}")
+        print(f"   Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"   Your final hand: {user_cards}, final score: {user_score}")
+    print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    os.system('clear')
+    play_game()
